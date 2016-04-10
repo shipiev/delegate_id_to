fdc0bfa5c3c4ead2af8e45472aa52c4cdf60c1e6
Roman Shipiev <roman@shipiev.pro>
2016-04-10 09:49:36 +0300

    fix error

--- lib/delegate_id_to.rb
+++ lib/delegate_id_to.rb
@@ -1,21 +1,24 @@
 module DelegateIdTo
-  def self.included(base) # :nodoc:
-    base.class_eval do
-      def self.delegate_id_to(*args)
-        args.each do |method_sym|
-          define_method(:"#{method_sym}_id") do
-            object = send(method_sym)
-            object && object.id
-          end
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def delegate_id_to(*args)
+      args.each do |method_sym|
+        define_method(:"#{method_sym}_id") do
+          object = send(method_sym)
+          object && object.id
         end
       end
+    end
 
-      def self.delegate_id_to_singular_associations
-        # @note For ActiveRecord::Base and children only
-        delegate_id_to *self.reflect_on_all_associations(:has_one).map(&:name)
-      end
+    def delegate_id_to_singular_associations
+      # @note For ActiveRecord::Base and children only
+      delegate_id_to *reflect_on_all_associations(:has_one).map(&:name)
     end
   end
+
 end
 
-Kernel.send(:include, DelegateIdTo) unless Kernel.included_modules.include? DelegateIdTo
+Object.send(:include, DelegateIdTo) unless Object.included_modules.include? DelegateIdTo
--- lib/delegate_id_to/version.rb
+++ lib/delegate_id_to/version.rb
@@ -1,3 +1,3 @@
 module DelegateIdTo
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 end
